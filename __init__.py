23da6998f390d947e8b3e4923a0c1f82259ea49e
Raimon Esteve <raimon@nan-tic.com>
2018-06-22 09:31:02 +0200

    Not register classes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,2 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import Pool
-
-def register():
-    Pool.register(
-        module='account_invoice_line_description', type_='model')
